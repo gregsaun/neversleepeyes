e7cdbdb1d9225c5e533f027e4a61bde3a831aa5e
gregsaun <gregoire.saunier@ekunn.com>
2016-12-22 17:24:16 +0100

    use gphoto2-updater. improve logging and check python-gphoto2 installation

--- install/install.sh
+++ install/install.sh
@@ -1,84 +1,71 @@
 #!/bin/bash
+################################################################################
+#
+# Description:
+# Bash script to install all necessary tools
+#
+# Author: Gregoire Saunier
+# Website: www.ekunn.com
+# Creation date: 2016.12.22
+#
+# Resources:
+#   - http://www.gphoto.org
+#   - https://github.com/gphoto/
+#   - https://github.com/gonzalo/gphoto2-updater
+#   - https://github.com/jim-easterbrook/python-gphoto2
+#
+################################################################################
 
-function raise_error {
-	echo "ERROR: ${1}"
-	exit -1
+
+GP_VERSION="2.5.11"      # Version of gphoto2 release on github to install
+LIBGP_VERSION="2.5.11.1" # Version of libgphoto2 used only to check installation
+
+
+function log {
+    echo -e "\n\n"
+    echo "====================================="
+    echo "${1}"
+    echo "====================================="    
 }
 
-# Packages for gphoto2 and libgphoto2 from normal repository
-#apt-get -y install python-pip python-dev gphoto2 libgphoto2-2-dev
 
-# Prepare installation of latest tarball release of gphoto2 and libgphoto2
-gp_version="2.5.11"
-echo "====================================="
-echo "Install lib for gphoto2 ${gp_version}"
-echo "====================================="
-sudo apt-get update
-sudo apt-get -y install libexif-dev libltdl-dev libtool libpopt-dev libaa1-dev libjpeg8-dev libcdk5-dev build-essential libudev-dev pkg-config gettext #automake autoconf autopoint
-wget -O libgphoto2-${gp_version}.tar.gz https://sourceforge.net/projects/gphoto/files/libgphoto/${gp_version}/libgphoto2-${gp_version}.tar.gz/download
-wget -O gphoto2-${gp_version}.tar.gz    https://sourceforge.net/projects/gphoto/files/gphoto/${gp_version}/gphoto2-${gp_version}.tar.gz/download
+function raise_error {
+    echo "ERROR: ${1}"
+    exit -1
+}
 
 
-# Install libgphoto2
-echo -e "\n\n"
-echo "====================================="
-echo "Start libgphoto2 installation"
-echo "====================================="
-tar -xvf libgphoto2-${gp_version}.tar.gz
-cd libgphoto2-${gp_version}/
-./configure && make && sudo make install
-if [ $? -ne 0 ]; then
-	raise_error "something went wrong during installation of libgphoto2!"
-fi
-echo "====================================="
-echo "End of libgphoto2 installation"
-echo "====================================="
-
-
-# Install gphoto2
-echo -e "\n\n"
-echo "====================================="
-echo "Start gphoto2 installation"
-echo "====================================="
-tar -xvf gphoto2-${gp_version}.tar.gz
-cd ../gphoto2-${gp_version}/
-./configure && make && sudo make install
-if [ $? -ne 0 ]; then
-	raise_error "something went wrong during installation of gphoto2!"
-fi
-echo "====================================="
-echo "End of gphoto2 installation"
-echo "====================================="
+# Install gphoto2 and libgphoto2 from github sources
+log "Install gphoto using gphoto2-updater"
+wget https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh && 
+chmod +x gphoto2-updater.sh && sudo ./gphoto2-updater.sh -d $GP_VERSION
 
 
 # Check gphoto2 install
-echo -e "\n\n"
-echo "====================================="
-echo "Version installed :"
+log "Check installation"
 gphoto2 --version > /dev/null
 if [ $? -ne 0 ]; then
-	raise_error "gphoto2 doesn't seems to be installed correctly!"
+        raise_error "gphoto2 doesn't seems to be installed correctly!"
 fi
 gphoto2 --version | tail -3
-if [ $(gphoto2 --version | tail -3 | awk '/^gphoto2 / {print $2}') != "${gp_version}" ]; then
-	raise_error "wrong version of gphoto2, should be ${gp_version}"
+if [ $(gphoto2 --version | tail -3 | awk '/^gphoto2 / {print $2}') != "${GP_VERSION}" ]; then
+        raise_error "wrong version of gphoto2, should be ${GP_VERSION}"
 fi
-if [ $(gphoto2 --version | tail -3 | awk '/^libgphoto2 / {print $2}') != "${gp_version}" ]; then
-	raise_error "wrong version of libgphoto2, should be ${gp_version}"
+if [ $(gphoto2 --version | tail -3 | awk '/^libgphoto2 / {print $2}') != "${LIBGP_VERSION}" ]; then
+        raise_error "wrong version of libgphoto2, should be ${LIBGP_VERSION}"
 fi
-echo "====================================="
-
-
-# Clean up
-cd ../
-rm -rf libgphoto2-${gp_version}.tar.gz gphoto2-${gp_version}.tar.gz libgphoto2-${gp_version} gphoto2-${gp_version}
+echo "gphoto2 and libgphoto2 are correctly installed :)"
 
 
 # Installation of python-gphoto2 (python binding for libgphoto2)
 # and other python modules
-echo -e "\n\n"
-echo "====================================="
-echo "Install python tools"
-echo "====================================="
-sudo apt-get -y install python-pip python-dev
-sudo pip install -r requirements.txt
+log "Install python tools"
+sudo apt-get update && sudo apt-get -y install python-pip python-dev
+sudo -H pip install -U pip
+sudo -H pip install -r requirements.txt
+python -c "import gphoto2 as gp; gp.check_result(gp.gp_context_new())"
+if [ $? -ne 0 ]; then
+        raise_error "python-gphoto2 doesn't seems to be installed correctly!"
+fi
+
+log "End of installation"
