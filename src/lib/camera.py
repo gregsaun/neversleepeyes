20ec7601e6c8bf20ef1563dede3a3f7a14eb6e82
gregsaun <gregoire.saunier@ekunn.com>
2016-12-23 15:23:57 +0100

    add method to get info from camera. Switch to non object usage of python-gphoto2 to be closer to gphoto2 API. Improve disconnection of camera

--- src/lib/camera.py
+++ src/lib/camera.py
@@ -1,4 +1,4 @@
-################################################################################
+###############################################################################
 #
 # Description:
 # Provide simplified camera interface. The camera is controlled with gphoto2
@@ -11,27 +11,27 @@
 #   - http://www.gphoto.org
 #   - https://github.com/jim-easterbrook/python-gphoto2
 #
-################################################################################
+###############################################################################
 
 # TODO :
-#  - Switch to non object usage of python-gphoto2 in order to be closer to libgphoto2 API
-#  - Summary should be a dict info like manufacturer, model, sn
+#  - more methods like capture()
 
 # Import
 from __future__ import print_function
 import gphoto2 as gp
 import logging
+import pprint
 
 
 class Camera(object):
     """ This represent a camera that can be manipulated (taking a photo, getting summary, etc.)
 
     Attributes:
-        name: name of a person (string)
+        dummy: todo description
     """
 
-    context = None
-    cam = None
+    cam_connected = False
+
 
     def __init__(self):
         """ Return a new MyClass object. """
@@ -39,38 +39,75 @@ class Camera(object):
             format='%(levelname)s: %(name)s: %(message)s', level=logging.ERROR)
         gp.check_result(gp.use_python_logging())
 
+
     def __del__(self):
         self.disconnect()
 
+
+    def __get_widget(self, camera_config):
+        """ Return dictionary containing all widget section values"""
+        cam_info = dict()
+
+        child_count = gp.check_result(gp.gp_widget_count_children(camera_config))
+
+        if child_count < 1:
+            return cam_info
+
+        for n in range(child_count):
+            child = gp.check_result(gp.gp_widget_get_child(camera_config, n))
+            #label = gp.check_result(gp.gp_widget_get_label(child))
+            name = gp.check_result(gp.gp_widget_get_name(child))
+            child_type = gp.check_result(gp.gp_widget_get_type(child))
+
+            if child_type == gp.GP_WIDGET_SECTION:
+                cam_info[name] = self.__get_widget(child)
+            else:
+                value = gp.check_result(gp.gp_widget_get_value(child))
+                cam_info[name] = value
+
+        return cam_info
+
+
     def connect(self):
         """ Return true if connection to camrea is good """
-        self.context = gp.Context()
-        self.cam = gp.Camera()
         try:
-            self.cam.init(self.context)
+            self.context = gp.gp_context_new()
+            self.cam = gp.check_result(gp.gp_camera_new())
+            gp.check_result(gp.gp_camera_init(self.cam, self.context))
+            self.cam_connected = True
+            return True
+
         except gp.GPhoto2Error as ex:
-            if ex.code == gp.GP_OK:
-                return True
+            self.disconnect()
+            self.cam_connected = False
+
+            if ex.code == gp.GP_ERROR_CAMERA_BUSY:
+                raise ValueError("Unable to connect camera. Camera is busy!")
+            elif ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
+                raise ValueError("Unable to connect camera. Camera not found!")
             else:
-                if ex.code == gp.GP_ERROR_CAMERA_BUSY:
-                    raise ValueError("Unable to connect camera. Camera is busy!")
-                elif ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
-                    raise ValueError("Unable to connect camera. Camera not found!")
-                else:
-                    raise ValueError("Unable to connect camera. Unknown error!")
-                return False
+                raise ValueError("Unable to connect camera. Unknown error!")
+
+            return False
+
 
     def disconnect(self):
         """ Return true if disconnection of the camera is good"""
-        self.cam.exit(self.context)
+        if self.cam_connected:
+            gp.gp_camera_exit(self.cam, self.context)
+            self.cam_connected = False
 
-    def get_summary(self):
-        """ Return multiline string containing summary of the camera"""
-        return self.cam.get_summary(self.context)
+
+    def get_info(self):
+        """ Return dictionary containing all camera info"""
+        cam_config = gp.check_result(gp.gp_camera_get_config(self.cam, self.context))
+        cam_info = self.__get_widget(cam_config)
+        return cam_info
 
 
 cam = Camera()
-
 cam.connect()
-print(cam.get_summary())
+cam_info = cam.get_info()
+print(cam_info["status"]["manufacturer"])
+pprint.pprint(cam_info)
 cam.disconnect()
