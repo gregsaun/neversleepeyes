71d8a1216eb20cbd711cc921dfd86c06624b847d
gregsaun <gregoire.saunier@ekunn.com>
2016-12-23 15:22:55 +0100

    use 2.5.10 instead of 2.5.11

--- install/install.sh
+++ install/install.sh
@@ -17,8 +17,8 @@
 ################################################################################
 
 
-GP_VERSION="2.5.11"      # Version of gphoto2 release on github to install
-LIBGP_VERSION="2.5.11.1" # Version of libgphoto2 used only to check installation
+GP_VERSION_CHECK="2.5.10"    # Version of gphoto2 used only to check installation
+LIBGP_VERSION_CHECK="2.5.10" # Version of libgphoto2 used only to check installation
 
 
 function log {
@@ -38,7 +38,7 @@ function raise_error {
 # Install gphoto2 and libgphoto2 from github sources
 log "Install gphoto using gphoto2-updater"
 wget https://raw.githubusercontent.com/gonzalo/gphoto2-updater/master/gphoto2-updater.sh && 
-chmod +x gphoto2-updater.sh && sudo ./gphoto2-updater.sh -d $GP_VERSION
+chmod +x gphoto2-updater.sh && sudo ./gphoto2-updater.sh -s
 
 
 # Check gphoto2 install
@@ -48,11 +48,11 @@ if [ $? -ne 0 ]; then
         raise_error "gphoto2 doesn't seems to be installed correctly!"
 fi
 gphoto2 --version | tail -3
-if [ $(gphoto2 --version | tail -3 | awk '/^gphoto2 / {print $2}') != "${GP_VERSION}" ]; then
-        raise_error "wrong version of gphoto2, should be ${GP_VERSION}"
+if [ $(gphoto2 --version | tail -3 | awk '/^gphoto2 / {print $2}') != "${GP_VERSION_CHECK}" ]; then
+        raise_error "wrong version of gphoto2, should be ${GP_VERSION_CHECK}"
 fi
-if [ $(gphoto2 --version | tail -3 | awk '/^libgphoto2 / {print $2}') != "${LIBGP_VERSION}" ]; then
-        raise_error "wrong version of libgphoto2, should be ${LIBGP_VERSION}"
+if [ $(gphoto2 --version | tail -3 | awk '/^libgphoto2 / {print $2}') != "${LIBGP_VERSION_CHECK}" ]; then
+        raise_error "wrong version of libgphoto2, should be ${LIBGP_VERSION_CHECK}"
 fi
 echo "gphoto2 and libgphoto2 are correctly installed :)"
 rm -f gphoto2-updater.sh*
@@ -68,5 +68,6 @@ python -c "import gphoto2 as gp; gp.check_result(gp.gp_context_new())"
 if [ $? -ne 0 ]; then
         raise_error "python-gphoto2 doesn't seems to be installed correctly!"
 fi
+
 
 log "End of installation"
